2e55081f5a8a3efa678360fd8c64da4d5e9f827b
Acer <Acer@LAPTOP-CNQS0KN4.speedport.ip>
2022-05-09 22:37:06 +0200

    Aufgabe 1 und 3 erledigt

--- src/idh/java/MyLinkedList.java
+++ src/idh/java/MyLinkedList.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 
 public class MyLinkedList<T> implements List<T> {
@@ -15,10 +16,23 @@ public class MyLinkedList<T> implements List<T> {
 	ListElement first;
 	
 	
+	/**
+	 *  It returns the number of elements of the list.
+	 */
+	
 	@Override
 	public int size() {
 		// TODO Implement!
-		return 0;
+		int size = 0;
+		if (first == null) {
+			return size = 0;
+		}
+		ListElement current = first;
+		while (current.next != null) {
+			size++;
+			current = current.next;
+		}
+		return size;
 	}
 
 	@Override
@@ -26,9 +40,22 @@ public class MyLinkedList<T> implements List<T> {
 		return first == null;
 	}
 
+	
+	/**
+	 * It checks whether the given item is present in the list or not.
+	 *  If the item is present then it returns true else false.
+	 */
+	
 	@Override
 	public boolean contains(Object o) {
 		// TODO Implement!
+		ListElement current = first;
+
+		while (current.next != null) {
+			if (current.equals(o))
+				return true;
+			current = current.next;
+		}
 		return false;
 	}
 
@@ -52,10 +79,24 @@ public class MyLinkedList<T> implements List<T> {
 		};
 	}
 
+	/**
+	 * Returns an array containing all of the elements in this
+	 *  list in proper sequence (from first to last element).
+	 */
+	
 	@Override
 	public Object[] toArray() {
 		// TODO Implement!
-		return null;
+		Object[] objArr = new Object[this.size()];
+		ListElement current = first;
+		for (int i = 0; i < objArr.length; i++) {
+			while (current.next != null) {
+				current = current.next;
+				objArr[i] = this.get(i);
+			}
+		}
+
+		return objArr;
 	}
 
 	@Override
@@ -80,10 +121,21 @@ public class MyLinkedList<T> implements List<T> {
 		return true;
 	}
 
+	/**
+	 * It removes the specified object from the list.
+	 * Returns true if this list contained the specified element
+	 */
+	
 	@Override
 	public boolean remove(Object o) {
 		// TODO: Implement
+		if (this.contains(o)) {
+			o = null;
+			return true;
+		}
+
 		return false;
+
 	}
 
 	@Override
@@ -101,6 +153,14 @@ public class MyLinkedList<T> implements List<T> {
 		return true;
 	}
 
+	/**
+	 * It adds all the elements of collection c to
+	 * the list starting from a give index in the list.
+	 * It throws NullPointerException if the collection c
+	 * is null and IndexOutOfBoundsException when the
+	 * specified index is out of the range.
+	 */
+	
 	@Override
 	public boolean addAll(int index, Collection<? extends T> c) {
 		// TODO Implement!
@@ -130,32 +190,98 @@ public class MyLinkedList<T> implements List<T> {
 		return getElement(index).value;
 	}
 
+	/**
+	 * It updates the item of specified index with the give value.
+	 */
+	
 	@Override
 	public T set(int index, T element) {
 		// TODO: Implement
+		if (first == null) {
+			throw new NoSuchElementException(); 
+		}else {
+			ListElement current = first ; 
+			while(current.next != null && index > 0 ) {
+				current = current.next;  
+				index--; 
+			}
+			if(index == 0)
+			current.value = element; 
+			
+		}
+		
 		return null;
 	}
 
+	/**
+	 *  It adds an item at the given index of the the list.
+	 */
+	
 	@Override
 	public void add(int index, T element) {
 		// TODO: Implement
+		first = new ListElement(element);
 	}
 
+	/**
+	 * It removes the item from the list which is present at the specified index.
+	 */
+	
 	@Override
 	public T remove(int index) {
 		// TODO: Implement
+		ListElement current = first;
+		int n = 0; 
+		while(current.next != first) {
+			if(n == index) {
+				current.value = null; 
+				return (T) current; 
+			}
+			
+			n++; 
+			current = current.next; 
+		}
 		return null;
 	}
 
+	/**
+	 *  It returns the index of the specified item.
+	 */
+	
 	@Override
 	public int indexOf(Object o) {
 		// TODO: Implement
-		return 0;
+		ListElement current = first;
+		int number = 0; 
+		while(current.next != null) {
+			if( current.value == o) {
+				return number; 
+			}
+			number++; 
+			current = current.next; 
+		}
+			return -1;
 	}
 
+	/**
+	 * It returns the index of last occurrence of the specified element.
+	 */
+	
 	@Override
 	public int lastIndexOf(Object o) {
 		// TODO: Implement
+		ListElement current = first; 
+		int i= 0 ; 
+		while(current.next != null) {
+			if(current.value == o) {
+				if(current.next.value == o) {
+					return i+1;
+				}
+				return i; 
+			}
+			i++; 
+			 
+		}
 		return 0;
 	}
 
@@ -276,6 +402,8 @@ public class MyLinkedList<T> implements List<T> {
 		ll.add("Hallo");
 		ll.add("Welt");
 		ll.get(0);
+		//System.out.println(ll.size());
+		//System.out.println(ll.contains("World"));
 		for (String s : ll) {
 			System.out.println(s);
 		}
