58512360697757034d2704d6a0b4bc54e40fb4ff
UntoastedToast <UntoastedToast@github.com>
2022-05-10 14:54:18 +0200

    Task 1 completed

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -13,20 +13,23 @@ import java.util.*;
 public class Document implements Iterable<String> {
 	String documentText;
 
+	// Creation of an ArrayList for the individual tokens
+	static List<String> txtTokens = new ArrayList<>();
+
 	public static Document readFromFile(File f) throws IOException {
 		FileReader fileReader = new FileReader(f);
 		int ch;
 		StringBuilder b = new StringBuilder();
-		while( (ch = fileReader.read()) != -1 ) {
+		while ((ch = fileReader.read()) != -1) {
 			b.append((char) ch);
 		}
 		fileReader.close();
 		Document doc = new Document();
 		doc.documentText = b.toString();
-		
+
 		return doc;
 	}
-	
+
 	public String getDocumentText() {
 		return documentText;
 	}
@@ -34,15 +37,26 @@ public class Document implements Iterable<String> {
 	public void setDocumentText(String documentText) {
 		this.documentText = documentText;
 	}
-	
+
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("data/dracula.txt"));
+
+		// Add Tokens to ArrayList
+		StringTokenizer tok = new StringTokenizer(d.documentText);
+
+		while (tok.hasMoreTokens()) {
+			txtTokens.add(tok.nextToken());
+		}
+
+		Iterator<String> iter = txtTokens.iterator();
+		while (iter.hasNext()) {
+			System.out.println(iter.next());
+		}
 	}
 
 	@Override
 	public Iterator<String> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return iterator();
 	}
-	
+
 }
