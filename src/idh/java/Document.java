b4a03fd8085020be03cdee78610f367fe5e376c6
BMusolff <92873072+BMusolff@users.noreply.github.com>
2022-05-10 19:11:51 +0200

    Exercise5

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -1,10 +1,11 @@
 package idh.java;
 
 import java.io.File;
+import java.util.Iterator;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Document {
+public class Document implements Iterable<String> {
 	String documentText;
 
 	public static Document readFromFile(File f) throws IOException {
@@ -31,6 +32,24 @@ public class Document {
 	
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("data/dracula.txt"));
+	}
+	
+	public String[] AusgabeTokens() {
+		ArrayList<String> eineListe = new ArrayList<String>();
+		StringTokenizer st = new StringTokenizer(documentText);
+		
+		try {
+	     while (st.hasMoreTokens()) {
+	    	 eineListe.add(st.nextToken());
+	     }}  catch(ArrayIndexOutOfBoundsException e) {
+	    	 System.out.println("ne");
+	     }
+		String[] words = new String[eineListe.size()];
+		for(int i = 0; i < eineListe.size(); i++) {
+			words[i] = eineListe.get(i);
+		}
+	     
+	     return words;
 	}
 	
 }
